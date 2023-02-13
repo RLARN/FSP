fa35398f9af1d6f603c7026e06925bf7210f6bb2
RLARN <msjdoc0645@naver.com>
2023-02-14 00:28:53 +0900

    더미데이터추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Article(id, title, content) VALUE(1, '가가가가', '1111');
+INSERT INTO Article(id, title, content) VALUE(2, '나나나나', '2222');
+INSERT INTO Article(id, title, content) VALUE(3, '다다다다', '3333');
